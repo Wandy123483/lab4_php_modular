db20bae4620e2ab42571301eec1c7fc3dd2c77c8
Wandy123483 <149190599+Wandy123483@users.noreply.github.com>
2024-05-02 22:21:12 +0700

    Add files via upload

---
+++ kontak.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman Kontak Dari Test Website Wandy Tarigan</h2>
+<p>Ini adalah bagian content dari halaman Kontak Website Wandy.</p>
+</div>
+<?php require('footer.php'); ?>
